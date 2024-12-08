c326fc4abe114ae816441f7087ec1df18e4ed6cc
Daniel Zhou <danielzh08@gmail.com>
2024-12-08 16:37:53 -0500

    update gitaction

--- lib/env_config.dart
+++ lib/env_config.dart
@@ -10,7 +10,47 @@ class EnvConfig {
       return dotenv.env[name].toString();
     }
     else {
-      return String.fromEnvironment(name, defaultValue: '');
+      // Release mode: Explicitly check for known environment variables
+      switch (name) {
+        case 'FIREBASE_API_KEY_WEB':
+          return const String.fromEnvironment('FIREBASE_API_KEY_WEB', defaultValue: '');
+        case 'FIREBASE_API_KEY_ANDROID':
+          return const String.fromEnvironment('FIREBASE_API_KEY_ANDROID', defaultValue: '');
+        case 'FIREBASE_API_KEY_IOS':
+          return const String.fromEnvironment('FIREBASE_API_KEY_IOS', defaultValue: '');
+        case 'FIREBASE_API_KEY_MACOS':
+          return const String.fromEnvironment('FIREBASE_API_KEY_MACOS', defaultValue: '');
+        case 'FIREBASE_API_KEY_WINDOWS':
+          return const String.fromEnvironment('FIREBASE_API_KEY_WINDOWS', defaultValue: '');
+
+        case 'FIREBASE_APP_ID_WEB':
+          return const String.fromEnvironment('FIREBASE_APP_ID_WEB', defaultValue: '');
+        case 'FIREBASE_APP_ID_ANDROID':
+          return const String.fromEnvironment('FIREBASE_APP_ID_ANDROID', defaultValue: '');
+        case 'FIREBASE_APP_ID_IOS':
+          return const String.fromEnvironment('FIREBASE_APP_ID_IOS', defaultValue: '');
+        case 'FIREBASE_APP_ID_MACOS':
+          return const String.fromEnvironment('FIREBASE_APP_ID_MACOS', defaultValue: '');
+        case 'FIREBASE_APP_ID_WINDOWS':
+          return const String.fromEnvironment('FIREBASE_APP_ID_WINDOWS', defaultValue: '');
+        
+        case 'FIREBASE_MESSAGE_SENDER_ID':
+          return const String.fromEnvironment('FIREBASE_MESSAGE_SENDER_ID', defaultValue: '');
+        case 'FIREBASE_PROJECT_ID':
+          return const String.fromEnvironment('FIREBASE_PROJECT_ID', defaultValue: '');
+        case 'FIREBASE_MEASUREMENT_ID':
+          return const String.fromEnvironment('FIREBASE_MEASUREMENT_ID', defaultValue: '');
+        case 'FIREBASE_IOS_CLIENT_ID':
+          return const String.fromEnvironment('FIREBASE_IOS_CLIENT_ID', defaultValue: '');
+        case 'FIREBASE_IOS_BUNDLE_ID':
+          return const String.fromEnvironment('FIREBASE_IOS_BUNDLE_ID', defaultValue: '');
+
+        case 'OPENAI_API_KEY':
+          return const String.fromEnvironment('OPENAI_API_KEY', defaultValue: '');
+
+        default:
+          return ''; // Handle unknown variables gracefully
+      }
     }
   }
 
@@ -20,11 +60,11 @@ class EnvConfig {
   static String get firebaseApiKeyMacOS => getEnvVariable('FIREBASE_API_KEY_MACOS');
   static String get firebaseApiKeyWindows => getEnvVariable('FIREBASE_API_KEY_WINDOWS');
 
-  static String get firebaseAppIdWeb => getEnvVariable('FIREBASE_API_KEY_WEB');
-  static String get firebaseAppIdAndroid => getEnvVariable('FIREBASE_API_KEY_ANDROID');
-  static String get firebaseAppIdIos => getEnvVariable('FIREBASE_API_KEY_IOS');
-  static String get firebaseAppIdMacOS => getEnvVariable('FIREBASE_API_KEY_MACOS');
-  static String get firebaseAppIdWindows => getEnvVariable('FIREBASE_API_KEY_WINDOWS');
+  static String get firebaseAppIdWeb => getEnvVariable('FIREBASE_APP_ID_WEB');
+  static String get firebaseAppIdAndroid => getEnvVariable('FIREBASE_APP_ID_ANDROID');
+  static String get firebaseAppIdIos => getEnvVariable('FIREBASE_APP_ID_IOS');
+  static String get firebaseAppIdMacOS => getEnvVariable('FIREBASE_APP_ID_MACOS');
+  static String get firebaseAppIdWindows => getEnvVariable('FIREBASE_APP_ID_WINDOWS');
 
   static String get firebaseMessagingSenderId => getEnvVariable('FIREBASE_MESSAGE_SENDER_ID');
   static String get firebaseProjectId => getEnvVariable('FIREBASE_PROJECT_ID');
